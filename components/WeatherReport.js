975c253603947e33fa067caa3add66cc62aa4bc1
unknown <JZ56@INLTD10036.global.tesco.org>
2015-11-07 23:02:55 +0000

    more info added

--- components/WeatherReport.js
+++ components/WeatherReport.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import MoreInfo from './MoreInfo'
 export default class WeatherReport extends React.Component {
   render() {
     
  return (
+ 	
          <div className="report-main">
            <span className="report">Current temperature in 
            <p>{this.props.city}</p>
@@ -10,6 +12,9 @@ export default class WeatherReport extends React.Component {
            </p>
            </span>
            <a href='http://localhost:5000/'> &lt; &lt; Go Back </a> 
+            <div className="info">
+             <MoreInfo  main={this.props.body.main} description={this.props.body.weather[0].description} country={this.props.body.sys.country}/> 
+             </div>        
  		</div>
         );
   }
